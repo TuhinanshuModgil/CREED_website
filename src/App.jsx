03020ce170a8a46921656ba5a2ccbaeb27fac815
Tuhinanshu Modgil <tuhinasnhumodgil18@gmail.com>
2024-06-09 17:55:49 +0530

    home page skeleton complete

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <AboutUs/>
+      <OurTeam/>
+      <FAQ/>
+      <Footer/>
+    </div>
+  )
+}
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import AboutUs from './components/AboutUs'
+import OurTeam from './components/OurTeam'
+import FAQ from './components/FAQ'
+import Footer from './components/Footer'
+
+export default App
